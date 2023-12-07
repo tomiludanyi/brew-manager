5b82f1f4a69371af9ae58a8c9891b6412734ba18
tomiludanyi <ludanyi.tamas@novaservices.hu>
2023-12-07 10:47:13 +0100

    the single 'ea' unit is removed from db; quantity conversion logic added

--- src/app/ingredients/ingredient-handler/ingredient-handler.component.ts
+++ src/app/ingredients/ingredient-handler/ingredient-handler.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { of, Subject, switchMap, takeUntil } from "rxjs";
+import { Ingredient } from "../ingredient.model";
 import { IngredientService } from "../ingredient.service";
 
 @Component({
@@ -7,12 +10,16 @@ import { IngredientService } from "../ingredient.service";
     templateUrl: './ingredient-handler.component.html',
     styleUrls: ['./ingredient-handler.component.css']
 })
-export class IngredientHandlerComponent implements OnInit {
+export class IngredientHandlerComponent implements OnInit, OnDestroy {
     ingredientForm!: FormGroup;
     ingredients$ = this.ingredientService.ingredients$;
     availableUnits: string[] = [];
+    selectedIngredient!: Ingredient;
+    private destroy$ = new Subject<void>();
     
-    constructor(private fb: FormBuilder, private ingredientService: IngredientService) {
+    constructor(private fb: FormBuilder,
+                private ingredientService: IngredientService,
+                private router: Router) {
     }
     
     ngOnInit(): void {
@@ -35,18 +42,60 @@ export class IngredientHandlerComponent implements OnInit {
         });
     }
     
-    onOptionChange(event: any) {
-        const selectedIngredientName = event.target.value;
+    onOptionChange() {
+        const selectedIngredientName = this.ingredientForm.get('name')?.value.name;
         
-        this.ingredients$.subscribe(ingredients => {
-            const selectedIngredient = ingredients.find(ingredient => ingredient.name === selectedIngredientName);
-            
-            if (selectedIngredient) {
-                const selectedUnit = selectedIngredient.unit;
-            }
-        });
+        this.ingredientService.getIngredientByName(selectedIngredientName)
+            .pipe(
+                takeUntil(this.destroy$),
+                switchMap(ingredient => {
+                    if (ingredient) {
+                        this.selectedIngredient = ingredient;
+                    }
+                    return of(null);
+                })
+            )
+            .subscribe();
     }
     
     onSubmit() {
+        const updatedIngredient: Ingredient = {
+            id: this.selectedIngredient?.id,
+            name: this.selectedIngredient?.name,
+            unit: this.selectedIngredient?.unit,
+            stock: this.selectedIngredient.stock
+        };
+        if (this.selectedIngredient?.unit === this.ingredientForm.get('unit')?.value) {
+            updatedIngredient.stock += this.ingredientForm.get('stock')?.value;
+            this.updateIngredientAndNavigate(updatedIngredient);
+        } else if (this.selectedIngredient.unit === 'g' && this.ingredientForm.get('unit')?.value === 'kg') {
+            updatedIngredient.stock += this.ingredientForm.get('stock')?.value * 1000;
+            this.updateIngredientAndNavigate(updatedIngredient);
+        } else if (this.selectedIngredient.unit === 'kg' && this.ingredientForm.get('unit')?.value === 'g') {
+            updatedIngredient.stock += this.ingredientForm.get('stock')?.value / 1000;
+            this.updateIngredientAndNavigate(updatedIngredient);
+        } else if (this.selectedIngredient.unit === 'ml' && this.ingredientForm.get('unit')?.value === 'l') {
+            updatedIngredient.stock += this.ingredientForm.get('stock')?.value * 1000;
+            this.updateIngredientAndNavigate(updatedIngredient);
+        } else if (this.selectedIngredient.unit === 'l' && this.ingredientForm.get('unit')?.value === 'ml') {
+            updatedIngredient.stock += this.ingredientForm.get('stock')?.value / 1000;
+            this.updateIngredientAndNavigate(updatedIngredient);
+        } else {
+            alert('The selected unit is not applicable for this ingredient!');
+        }
+    }
+    
+    private updateIngredientAndNavigate(updatedIngredient: Ingredient) {
+        this.ingredientService.updateIngredient(updatedIngredient)
+            .pipe(
+                switchMap(() => this.router.navigate(['ingredient-list'])),
+                takeUntil(this.destroy$)
+            )
+            .subscribe();
+    }
+    
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 }
