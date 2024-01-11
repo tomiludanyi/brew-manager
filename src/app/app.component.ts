5744624da6f1266ede21d53fd3b30f87e0178b25
tomiludanyi <ludanyi.tamas@novaservices.hu>
2024-01-11 17:09:44 +0100

    style updated for the whole app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'brew-manager';
+    
+    isRouteActive!: boolean;
+    
+    constructor(private router: Router) {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                this.isRouteActive = this.router.url !== '/';
+            }
+        });
+    }
 }
